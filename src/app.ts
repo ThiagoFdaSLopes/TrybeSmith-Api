2650d5945d275e8c34a756a5a07fb4af86570b7c
Thiago <programadorthiagolopes@gmail.com>
2023-02-06 15:22:10 -0300

    feat: Adicionado rota products

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import productRouter from './routes/products.router';
 
 const app = express();
 
 app.use(express.json());
+app.use('/products', productRouter);
 
 // Iniciando Projeto
 
