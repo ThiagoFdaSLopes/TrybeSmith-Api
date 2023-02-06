3e563453a58cef8ca62dc44db6738525ff1a2741
Thiago <programadorthiagolopes@gmail.com>
2023-02-06 18:15:29 -0300

    chore: Add validate Token

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -5,3 +5,8 @@ export default function createToken(body: Payload): string {
   const token = jwt.sign(body, 'secret');
   return token;
 }
+
+// export function validateTokenUser(token: string) {
+//   const verify = jwt.verify(token, 'secret');
+//   return verify;
+// }
