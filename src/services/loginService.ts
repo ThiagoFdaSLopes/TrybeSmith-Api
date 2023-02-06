8a180b7e31e883cf2d81cee199502d54e038b486
Thiago <programadorthiagolopes@gmail.com>
2023-02-06 18:14:41 -0300

    feat: Service Login

---
+++ src/services/loginService.ts
@@ -0,0 +1,21 @@
+import connection from '../models/connection';
+import LoginModel from '../models/loginModel';
+import { ILogin } from '../interfaces';
+
+export default class LoginService {
+  loginModel: LoginModel;
+
+  constructor() {
+    this.loginModel = new LoginModel(connection);
+  }
+
+  async userExist(body: ILogin) {
+    try {
+      const result = await this.loginModel.userExist(body);
+      if (!result) throw new Error();
+      return result;
+    } catch (error) {
+      throw new Error('Usuario ou login incorreto');
+    }
+  }
+}
