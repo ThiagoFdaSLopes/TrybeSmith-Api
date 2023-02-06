b3c156ada32689e3fbee15265070ce6761f50921
Thiago <programadorthiagolopes@gmail.com>
2023-02-06 19:03:15 -0300

    feat: middlewares na rota users

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,10 +1,21 @@
 import express from 'express';
 import UserController from '../controllers/userController';
+import validateUserName from '../middlewares/validateUserName';
+import validatePassword from '../middlewares/validatePassword';
+import validateVocation from '../middlewares/validateVocation';
+import validateLevel from '../middlewares/validateLevel';
 
 const userController = new UserController();
 
 const router = express.Router();
 
-router.post('/', userController.createUser);
+router.post(
+  '/',
+  validateUserName,
+  validateVocation,
+  validateLevel,
+  validatePassword,
+  userController.createUser,
+);
 
 export default router;
