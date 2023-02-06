c12822e0b530d9f52219d5ef8b2edf1cf97924a5
Thiago <programadorthiagolopes@gmail.com>
2023-02-06 17:35:17 -0300

    feat: Criado router para Orders

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import productRouter from './routes/products.router';
 import userRouter from './routes/user.router';
+import orderRouter from './routes/order.router';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', productRouter);
 app.use('/users', userRouter);
+app.use('/orders', orderRouter);
 
 // Iniciando Projeto
 
