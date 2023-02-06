1437cc89f707fd70b3b97db719db1955c266d48b
Thiago <programadorthiagolopes@gmail.com>
2023-02-06 15:23:08 -0300

    feat: Criado connection.ts

---
+++ src/models/connection.ts
@@ -0,0 +1,9 @@
+import mysql from 'mysql2/promise';
+
+const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+});
+
+export default connection;
