bd1f6e7efa9c10192cf40fb7c24257fa03283a6a
Thiago <programadorthiagolopes@gmail.com>
2023-02-06 15:34:45 -0300

    feat: Criado rota get para getAllProducts

--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -6,5 +6,6 @@ const productController = new ProductController();
 const router = express.Router();
 
 router.post('/', productController.createProduct);
+router.get('/', productController.getAllProducts);
 
 export default router;
