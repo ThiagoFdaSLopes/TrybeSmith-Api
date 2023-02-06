36040a2c00e54ed4707d2f17425f514a5fea465f
Thiago <programadorthiagolopes@gmail.com>
2023-02-06 17:02:49 -0300

    feat: Criado rota users

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import productRouter from './routes/products.router';
+import userRouter from './routes/user.router';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', productRouter);
+app.use('/users', userRouter);
 
 // Iniciando Projeto
 
