12b999778288a7c87519b7f1c6ad5d8aac158881
Thiago <programadorthiagolopes@gmail.com>
2023-02-06 15:23:19 -0300

    feat: Criado model para Products com class

---
+++ src/models/productModel.ts
@@ -0,0 +1,18 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { IProductCreate } from '../interfaces';
+
+export default class ProductModel {
+  connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  async createProduct(body: IProductCreate) {
+    const [{ insertId }] = await
+    this.connection.execute<ResultSetHeader>(`
+    INSERT INTO Trybesmith.products (name, amount) VALUES (?, ?)`, [body.name, body.amount]);
+
+    return insertId;
+  }
+}
