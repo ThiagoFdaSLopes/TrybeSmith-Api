19eaa56f3d982a4102c08a00982e091f870fe8b0
Thiago <programadorthiagolopes@gmail.com>
2023-02-06 15:23:38 -0300

    feat: Service para products

---
+++ src/services/productService.ts
@@ -0,0 +1,20 @@
+import { IProductCreate } from '../interfaces';
+import connection from '../models/connection';
+import ProductModel from '../models/productModel';
+
+export default class ProductService {
+  productModel: ProductModel;
+
+  constructor() {
+    this.productModel = new ProductModel(connection);
+  }
+
+  async createProduct(body: IProductCreate) {
+    try {
+      const idProduct = await this.productModel.createProduct(body);
+      return { message: { id: idProduct, ...body } };
+    } catch (error) {
+      return { message: 'Erro na hora de criar produto' };
+    }
+  }
+}
